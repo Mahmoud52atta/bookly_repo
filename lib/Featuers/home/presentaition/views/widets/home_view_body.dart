cea133c0e511be53699fcbfbb4bd7df2cc9f8f51
Mahmoud52atta <github5257@gmail.com@example.com>
2024-09-26 15:16:33 +0300

    featuer boocks listView

--- lib/Featuers/home/presentaition/views/widets/home_view_body.dart
+++ lib/Featuers/home/presentaition/views/widets/home_view_body.dart
@@ -1,6 +1,5 @@
 import 'package:bookly_app/Featuers/home/presentaition/views/widets/custom_uppbar.dart';
-import 'package:bookly_app/Featuers/home/presentaition/views/widets/featuer_ListView_Item.dart';
-import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/Featuers/home/presentaition/views/widets/featuer_boocks_listView.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -13,29 +12,6 @@ class HomeViewBody extends StatelessWidget {
         CustomAppBar(),
         FeatuerBoocksListView(),
       ],
-    );
-  }
-}
-
-class FeatuerBoocksListView extends StatelessWidget {
-  const FeatuerBoocksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .32,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.only(
-              right: 10,
-              left: 8,
-            ),
-            child: const FeatuerListViewItem(),
-          );
-        },
-      ),
     );
   }
 }
