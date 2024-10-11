d5f114179a3871f8e831d4cb69a3bbe53ab79bff
Mahmoud52atta <github5257@gmail.com@example.com>
2024-10-11 17:15:53 +0300

    create failuer with dioError

--- lib/Featuers/home/data/rebos/home_rebo_impl.dart
+++ lib/Featuers/home/data/rebos/home_rebo_impl.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/Featuers/home/data/rebos/home_rebo.dart';
 import 'package:bookly_app/core/utils/apis_service.dart';
 import 'package:bookly_app/core/utils/errors/failuers.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeReboImpl implements HomeRebo {
   final ApisService apisService;
@@ -19,15 +20,36 @@ class HomeReboImpl implements HomeRebo {
       }
       return right(books);
     } catch (e) {
+      if (e is DioException) {
+        return left(ServesFailuer.fromDioError(e));
+      }
       return left(
-        ServesFailuer(),
+        ServesFailuer(
+          errorMessage: e.toString(),
+        ),
       );
     }
   }
 
   @override
-  Future<Either<Failuers, List<BookModel>>> fetchFeatuersBooks() {
-    // TODO: implement fetchFeatuersBooks
-    throw UnimplementedError();
+  Future<Either<Failuers, List<BookModel>>> fetchFeatuersBooks() async {
+    try {
+      var data = await apisService.get(
+          endPoint: 'volumes?Filtering=free-ebooks&q=subject:Programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServesFailuer.fromDioError(e));
+      }
+      return left(
+        ServesFailuer(
+          errorMessage: e.toString(),
+        ),
+      );
+    }
   }
 }
--- lib/core/utils/errors/failuers.dart
+++ lib/core/utils/errors/failuers.dart
@@ -1,3 +1,54 @@
-class Failuers {}
+import 'package:dio/dio.dart';
 
-class ServesFailuer extends Failuers {}
+class Failuers {
+  final String errorMessage;
+
+  Failuers({required this.errorMessage});
+}
+
+class ServesFailuer extends Failuers {
+  ServesFailuer({required super.errorMessage});
+
+  factory ServesFailuer.fromDioError(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServesFailuer(errorMessage: 'Connection timeout with ApiServer');
+      case DioExceptionType.sendTimeout:
+        return ServesFailuer(errorMessage: 'Send timeout with ApiServer');
+      case DioExceptionType.receiveTimeout:
+        return ServesFailuer(errorMessage: 'Receive timeout with ApiServer');
+      case DioExceptionType.badCertificate:
+        return ServesFailuer(errorMessage: 'BadCertificate with ApiServer');
+      case DioExceptionType.badResponse:
+        return ServesFailuer.fromRespnse(
+            dioError.response!.statusCode!, dioError.response!.data);
+      case DioExceptionType.cancel:
+        return ServesFailuer(errorMessage: 'ApisServer Was Cansel');
+
+      // TODO: Handle this case.
+      case DioExceptionType.connectionError:
+        return ServesFailuer(errorMessage: 'Error in Network');
+
+      // TODO: Handle this case.
+      case DioExceptionType.unknown:
+        return ServesFailuer(
+            errorMessage: 'Opps tere was an error,please try latter');
+
+      // TODO: Handle this case.
+    }
+  }
+  factory ServesFailuer.fromRespnse(int stutuseCode, dynamic response) {
+    if (stutuseCode == 400 || stutuseCode == 401 || stutuseCode == 403) {
+      return ServesFailuer(errorMessage: response['error']['message']);
+    } else if (stutuseCode == 404) {
+      return ServesFailuer(
+          errorMessage: 'Your Request is not found,please try latter');
+    } else if (stutuseCode == 500) {
+      return ServesFailuer(
+          errorMessage: 'Intern Server Error,please try latter');
+    } else {
+      return ServesFailuer(
+          errorMessage: 'Opps tere was an error,please try latter');
+    }
+  }
+}
